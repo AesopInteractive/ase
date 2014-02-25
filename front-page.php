db31c84817894af01cb1a4fd7f352480c5f86462
Bearded Avenger <email@nickhaskins.com>
2014-02-24 19:59:53 -0600

    check check

--- front-page.php
+++ front-page.php
@@ -7,16 +7,17 @@
 		<div class="ase-section ase-welcome">
 			<div class="ase-content">
 				<?php echo do_shortcode('[aesop_timeline_stop num="Aesop Story Engine"]');?>
-				<p>sdfsdf</p>
-				<a href="#" class="btn">Link</a>
-				<a href="#" class="btn">Link</a>
+				<p class="ase-sub-title">An easy-to-use set of tools that help you tell rich, interactive stories using an open-source WordPress plugin.</p>
+				<a href="#" class="btn btn-default ase-welcome-btn">Link</a>
+				<a href="#" class="btn btn-default ase-welcome-btn">Link</a>
 			</div>
 		</div>
 
 		<div class="ase-section ase-features">
 			<div class="ase-content clearfix">
 				<div class="ase-features-gallery col-md-8">
-					<img src="http://placekitten.com/600/400">
+
+					<?php echo do_shortcode('[aesop_gallery id="25"]');?>
 				</div>
 				<div class="ase-features-text col-md-4">
 					<?php echo do_shortcode('[aesop_timeline_stop num="Features"]');?>
@@ -46,15 +47,21 @@
 		<div class="ase-section ase-extras">
 			<div class="ase-content">
 				<?php echo do_shortcode('[aesop_timeline_stop num="Extras"]');?>
+				<div class="ase-extra">
+					<h4>Aesop Essentials</h4>
+					<img class="ase-section-img" src="http://placehold.it/400x250">
+					<p class="ase-sub-title">An add-on plugin that enables additional functionality within Aesop such as image lazy loading, Google Analytics integration, and more.</p>
+					<a href="#" class="btn btn-default">Get it</a>
+				</div>
 			</div>
 		</div>
 
 		<div class="ase-section ase-actions">
 			<div class="ase-content">
-				<?php echo do_shortcode('[aesop_timeline_stop num="Actions"]');?>
+				<h2>Download Aesop</h2>
 				<p>sdfsdf</p>
-				<a href="#" class="btn">Link</a>
-				<a href="#" class="btn">Link</a>
+				<a href="#" class="btn btn-default ase-welcome-btn">Link</a>
+				<a href="#" class="btn btn-default ase-welcome-btn">Link</a>
 			</div>
 		</div>
 
