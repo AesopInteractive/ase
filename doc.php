00cd8948159be6fb7bd3a84a1873da68ec6ba140
Bearded Avenger <email@nickhaskins.com>
2014-02-25 18:08:47 -0600

    more doc styling

--- doc.php
+++ doc.php
@@ -45,11 +45,14 @@ get_header();
 					</li>
 				</ul>
 
+				<hr />
 				<?php echo do_shortcode('[aesop_timeline_stop num="Gallery Component"]');?>
 
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi et turpis est. Cras id quam non neque elementum eleifend et sed metus. Cras vestibulum, turpis id semper aliquam, mi ligula cursus purus, ac mattis neque justo eget leo. Nunc suscipit scelerisque ligula, eu faucibus ipsum euismod ut. Duis et justo varius, iaculis libero vel, auctor tellus. Phasellus hendrerit lectus enim, nec molestie nulla scelerisque at. Etiam tempor tempus magna rutrum suscipit. Nunc venenatis euismod risus, nec pellentesque nisi venenatis rhoncus. Nulla facilisi.</p>
 
 				<p>Vivamus sit amet tortor vitae orci iaculis condimentum non quis magna. Nunc vehicula venenatis condimentum. Cras ac faucibus mauris, eget condimentum elit. In hac habitasse platea dictumst. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis sit amet leo gravida, ultrices dui at, vulputate nibh. Nulla et turpis lectus. Phasellus pulvinar sed lacus quis sollicitudin. Proin quis posuere tortor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum ullamcorper venenatis volutpat. In tincidunt dui augue, vel pretium odio pellentesque at. Proin nec leo viverra, viverra velit id, posuere massa. Pellentesque tellus nunc, iaculis ac tellus sit amet, scelerisque dapibus orci.</p>
+
+				<p class="ase-doc-note"><span class="label label-primary">tip</span> whatevers</p>
 
 				<p>Curabitur sodales augue at leo adipiscing posuere. Donec feugiat arcu at quam malesuada, et lacinia enim feugiat. Aenean urna felis, commodo laoreet mi in, laoreet egestas urna. Integer gravida lacinia nulla, in eleifend velit. Pellentesque sed turpis vel elit auctor tempor. Duis sodales convallis convallis. In varius condimentum quam ac aliquet. Vivamus luctus tellus sed commodo tincidunt. Aliquam tincidunt nisl malesuada neque placerat lobortis. Pellentesque tortor lorem, auctor in purus quis, elementum consectetur enim. Pellentesque pulvinar ipsum sed dictum fringilla. Cras eu nibh a libero aliquet tempus mattis in nunc. Nulla nec commodo neque.</p>
 
@@ -57,7 +60,7 @@ get_header();
 
 				<p>Morbi non dictum nunc. Pellentesque pharetra at arcu quis iaculis. Phasellus tincidunt convallis aliquam. Vivamus a quam metus. Suspendisse egestas quis ligula in ornare. Ut nec molestie quam. Nunc odio elit, gravida non ultricies sit amet, ullamcorper quis sem. Etiam a lacinia nisl, non dictum magna. Nullam dolor sapien, fermentum sit amet magna tristique, fermentum volutpat massa. Nulla facilisi. Aenean erat lorem, egestas eget nisl luctus, volutpat aliquet quam. Proin sit amet lacus vitae lorem facilisis mattis eget ut arcu. Donec elementum auctor dignissim.</p>
 
-
+				<hr />
 				<?php echo do_shortcode('[aesop_timeline_stop num="Quote Component"]');?>
 
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi et turpis est. Cras id quam non neque elementum eleifend et sed metus. Cras vestibulum, turpis id semper aliquam, mi ligula cursus purus, ac mattis neque justo eget leo. Nunc suscipit scelerisque ligula, eu faucibus ipsum euismod ut. Duis et justo varius, iaculis libero vel, auctor tellus. Phasellus hendrerit lectus enim, nec molestie nulla scelerisque at. Etiam tempor tempus magna rutrum suscipit. Nunc venenatis euismod risus, nec pellentesque nisi venenatis rhoncus. Nulla facilisi.</p>
