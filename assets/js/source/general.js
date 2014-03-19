52e384544fa3305becfb8a50a17ca3122e1d7cba
Bearded Avenger <email@nickhaskins.com>
2014-03-19 08:20:54 -0500

    check nav

--- assets/js/source/general.js
+++ assets/js/source/general.js
@@ -18,9 +18,9 @@ jQuery(document).ready(function(){
 jQuery('.ase-primary-nav,.ase-sticky-nav-menu').append('<nav class="ase-secondary-nav">\
 									<li><a href="">Blog</a></li>\
 									<li><a href="">Contact</a></li>\
-									<li><a href=""><i class="asecon asecon-twitter"></i></a></li>\
-									<li><a href=""><i class="asecon asecon-twitter"></i></a></li>\
-									<li><a href=""><i class="asecon asecon-twitter"></i></a></li>\
-									<li><a href=""><i class="asecon asecon-twitter"></i></a></li>\
+									<li class="ase-nav-social"><a href=""><i class="asecon asecon-twitter"></i></a></li>\
+									<li class="ase-nav-social"><a href=""><i class="asecon asecon-twitter"></i></a></li>\
+									<li class="ase-nav-social"><a href=""><i class="asecon asecon-twitter"></i></a></li>\
+									<li class="ase-nav-social"><a href=""><i class="asecon asecon-twitter"></i></a></li>\
 									</nav>
 									');
