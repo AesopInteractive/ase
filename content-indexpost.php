539ef1ab6cebd96fd441f54a6c44d8cbd01f7994
Bearded Avenger <email@nickhaskins.com>
2014-03-23 08:58:42 -0500

    blog single

--- content-indexpost.php
+++ content-indexpost.php
@@ -6,7 +6,7 @@ $imgsrc = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), "Full
 	<div class="ase-index-post-inner">
 		<?php the_title('<h2>', '</h2>');?>
 		<?php the_excerpt();?>
-		<a class="ase-index-post-link" href="<?php echo the_permalink();?>">Read this Story</a>
+		<a class="btn btn-primary ase-index-post-link" href="<?php echo the_permalink();?>">Read this Story <span>&rarr;</span></a>
 	</div>
 	<div class="ase-index-bg-img" style="background:url(<?php echo $imgsrc[0];?>);background-size:cover;background-position:center center;"></div>
 
