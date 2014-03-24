95e39c58758f185245b6b3a5d593261ee883657d
Bearded Avenger <email@nickhaskins.com>
2014-03-24 09:43:56 -0500

    single product stuff

--- single-download.php
+++ single-download.php
@@ -4,7 +4,7 @@ if(have_posts()) : while(have_posts()) : the_post(); ?>
 	<div class="ase-content clearfix">
 
 		<div class="col-md-9 ase-library-main">
-			<?php the_title('<h2>', '</h2>' );?>
+			<?php the_title('<h2 class="ase-product-title">', '</h2>' );?>
 			<?php echo the_content();
 
 			// If comments are open or we have at least one comment, load up the comment template.
