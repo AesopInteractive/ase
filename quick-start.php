4fdb6909a6f86106c3ff9017ca9ce56298360bdc
Bearded Avenger <email@nickhaskins.com>
2014-03-25 11:43:50 -0500

    quick start tweaks

--- quick-start.php
+++ quick-start.php
@@ -11,27 +11,26 @@ get_header();
 
 	<div class="ase-section ase-step-one">
 		<div class="ase-content clearfix">
-			<?php echo do_shortcode('[aesop_image img="/wp-content/themes/ase/assets/img/install-aesop.png" align="center" captionposition="left" lightbox="off" imgwidth="100%"]');?>
 			<div class="ase-step-inner">
 				<h2>Install Aesop Story Engine</h2>
 				<p>Head to the plugins tab within your WordPress administration area. Click on "add new". Once here, search for Aesop Story Engine, and press "search." Install Aesop Story Engine, and you're set! Hint: you can also install through FTP if you know that that is.</p>
 			</div>
+			<?php echo do_shortcode('[aesop_image img="/wp-content/themes/ase/assets/img/install-aesop.png" align="center" captionposition="left" lightbox="off" imgwidth="100%"]');?>
 		</div>
 	</div>
 
 	<div class="ase-section ase-step-two">
 		<div class="ase-content clearfix">
-			<?php echo do_shortcode('[aesop_image img="/wp-content/themes/ase/assets/img/add-component.png" align="center" captionposition="left" lightbox="off" imgwidth="100%"]');?>
 			<div class="ase-step-inner">
 				<h2>Build your Story</h2>
 				<p>Go to any post, and locate the "Add Component" button. It sits right above the post editor. Once you toggle this, you'll be able to select the component, adjust any options, and insert it anywhere within your story. Galleries are managed with the "Galleries" tab within WordPress.</p>
 			</div>
+			<?php echo do_shortcode('[aesop_image img="/wp-content/themes/ase/assets/img/add-component.png" align="center" captionposition="left" lightbox="off" imgwidth="100%"]');?>
 		</div>
 	</div>
 
 	<div class="ase-section ase-step-three ">
 		<div class="ase-content clearfix">
-			<?php echo do_shortcode('[aesop_image img="http://placekitten.com/800/250" align="center" captionposition="left" lightbox="off" imgwidth="100%"]');?>
 			<div class="ase-step-inner">
 				<h2>Check out Resources</h2>
 				<p>Search through our WordPress <a href="/library/category/themes">themes for Aesop</a> to get started with a site quickly, or <a href="/library/category/add-ons">browse our available add-ons</a> which enhance our themes and the Aesop Story Engine.</p>
