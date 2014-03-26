64048f880384ea38a0311cc0e99af6b3cfd46027
Bearded Avenger <email@nickhaskins.com>
2014-03-26 12:43:42 -0500

    pinterest identify

--- header.php
+++ header.php
@@ -15,6 +15,7 @@
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<link href='//fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,400italic' rel='stylesheet' type='text/css'>
+	<meta name="p:domain_verify" content="eab589912490f786548fbc933e13b516"/>
 	<!--[if lt IE 9]>
 	<script src="<?php echo get_template_directory_uri(); ?>/assets/js/source/html5.js"></script>
 	<![endif]-->
