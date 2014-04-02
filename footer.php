d93f725cd73e502846276bc403664ab270d16fae
Bearded Avenger <email@nickhaskins.com>
2014-04-01 19:17:22 -0500

    fb root div

--- footer.php
+++ footer.php
@@ -29,7 +29,7 @@
 		  ga('send', 'pageview');
 
 		</script>
-
+		<div id="fb-root"></div>
 		<script>(function(d, s, id) {
 		  var js, fjs = d.getElementsByTagName(s)[0];
 		  if (d.getElementById(id)) return;
