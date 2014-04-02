4f8c163f52a01302f5a0f9ff7664a8ebace87a4e
Bearded Avenger <email@nickhaskins.com>
2014-04-01 19:15:31 -0500

    fix single shrea?

--- footer.php
+++ footer.php
@@ -30,6 +30,14 @@
 
 		</script>
 
+		<script>(function(d, s, id) {
+		  var js, fjs = d.getElementsByTagName(s)[0];
+		  if (d.getElementById(id)) return;
+		  js = d.createElement(s); js.id = id;
+		  js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1&appId=492973480802758";
+		  fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));</script>
+
 		<?php do_action('aesop_inside_body_bottom');?>
 
 	</body>
