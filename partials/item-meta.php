5e5627db10a495f069b6ad8bc91984a2c0852d93
Bearded Avenger <email@nickhaskins.com>
2014-09-18 08:10:40 -0500

    if no demo button then center purchase button

--- partials/item-meta.php
+++ partials/item-meta.php
@@ -1,12 +1,13 @@
 <?php
 $demo       	=   get_post_meta( get_the_ID(), 'ase_item_demo', true);
+$nodemobtn      = empty($demo) ? 'single-btn' : null;
 ?>
 <div class="ase-stacked-title">
 	<?php the_title('<h1 class="ase-product-title">', '</h1>' );?>
 </div>
-<div class="ase-library-btn-group" itemprop="offers" itemscope itemtype="http://schema.org/Offer">
+<div class="ase-library-btn-group <?php echo $nodemobtn;?>" itemprop="offers" itemscope itemtype="http://schema.org/Offer">
 	<?php if ($demo){?>
-	<a href="<?php echo $demo;?>" class="btn btn-primary">Test Drive</a>
+	<a href="<?php echo $demo;?>" class="btn btn-primary">Create Personal Demo</a>
 	<?php } ?>
 	<?php echo edd_get_purchase_link($post->ID, 'Buy Now', 'button', 'blue');?>
 </div>
